20cc23af155daf2ce4a158c19248231ae76aec6b
Shubham <vishwakarmashubham202@gmail.com>
2025-06-06 20:55:36 +0530

    practice array related problems

---
+++ Calculator/script.js
@@ -0,0 +1,31 @@
+const students = [
+    {
+        name:'shubham',
+        age:22,
+        city:'Mumbai'
+    },
+
+    {
+        name:'shubham',
+        age:22,
+        city:'Mumbai'
+    },
+
+    {
+        name:'shubham',
+        age:22,
+        city:'Mumbai'
+    },
+
+    {
+        name:'shubham',
+        age:22,
+        city:'Mumbai'
+    }
+]
+
+students.forEach((student, index) => {
+    console.log(`Student ${index + 1}:`, student);
+});
+
+// console.log(students);
