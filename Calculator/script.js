2b62991bd6c6ce76c26d76155c1da6b08913e492
Shubham <vishwakarmashubham202@gmail.com>
2025-06-09 21:51:17 +0530

    topic updated

---
+++ Calculator/script.js
@@ -0,0 +1,18 @@
+// const detail = {
+//     name:'shubham',
+//     age:22,
+//     info:function(){
+//         console.log(this.name , this.age);
+        
+//     }
+// }
+
+// detail.info()
+// console.log();
+
+// function add(){
+//     console.log(this);
+// }
+
+// add()
+
