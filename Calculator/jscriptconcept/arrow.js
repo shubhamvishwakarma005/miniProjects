206f71a5586e86c9dc43d62df09998580539f2f9
Shubham <vishwakarmashubham202@gmail.com>
2025-06-09 21:50:47 +0530

    done some more topic

--- Calculator/jscriptconcept/arrow.js
+++ Calculator/jscriptconcept/arrow.js
@@ -1,16 +1,16 @@
-const person ={
-    name:'shubham',
-    age:22,
-    city:'mumbai',
-    hobbies:["coding","reading books","playing"],
-    playhobbies: function(){
-        this.hobbies.forEach((hobby) => {
-            console.log(this.name + "likes" + hobby);
-        });
-    }
-}
+// const person ={
+//     name:'shubham',
+//     age:22,
+//     city:'mumbai',
+//     hobbies:["coding","reading books","playing"],
+//     playhobbies: function(){
+//         this.hobbies.forEach((hobby) => {
+//             console.log(this.name + "likes" + hobby);
+//         });
+//     }
+// }
 
-person.playhobbies();
+// person.playhobbies();
 
 
 // const hobbies =["coding", "reading", "singing", "playing", "writing"]
@@ -18,3 +18,48 @@ person.playhobbies();
 // hobbies.forEach(hobby => {
 //     console.log(hobby)
 // });
+
+
+// const detail={
+//     name:'shubham',
+//     age:22,
+//     city:'mumbai',
+//     info : () => (this.name)
+// }
+// console.log(detail.info());
+
+
+// console.log(typeof this);
+
+// const person = {
+//   firstName  : "John",
+//   lastName   : "Doe",
+//   id         : 5566,
+//   myFunction : function() {
+//     return this;
+//   }
+// };
+
+// console.log(person.myFunction())
+
+
+
+// let myfunc = (a,b) => (a*b);
+
+// console.log(myfunc(5,8));
+
+
+// console.log('hii there')
+// setTimeout( () => (console.log('hello its setTimeout')), 3000);
+// console.log('how are you doing ');
+
+
+// console.log('hii there');
+
+// setInterval(()=> {
+//     console.log('hii setIntervel here !'); 
+// },3000)
+
+// console.log('how are you doing ');
+
+
