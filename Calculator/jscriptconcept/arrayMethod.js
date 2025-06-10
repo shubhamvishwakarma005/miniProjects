f816eb741d84db089bbc4d21934ce2aae2ff9190
Shubham <vishwakarmashubham202@gmail.com>
2025-06-10 21:38:52 +0530

    learn some new concepts

---
+++ Calculator/jscriptconcept/arrayMethod.js
@@ -0,0 +1,203 @@
+// const names=["shubham","anjali","dipesh","komal","sweety"];
+
+// names.forEach(function(name){
+//     console.log(name); 
+// });
+
+
+// names.forEach(name => {
+//     console.log(name);
+    
+// })
+
+
+// for (const name of names) {
+//     console.log(name);
+    
+// }
+
+
+// const students =[
+//     {
+//         name:'shubham',
+//         roll:'001'
+//     },
+//     {
+//         name:'anjali',
+//         roll:'002'
+//     },
+//     {
+//         name:'dipesh',
+//         roll:'003'
+//     },
+//     {
+//         name:'sweety',
+//         roll:'004'
+//     },
+//     {
+//         name:'kajal',
+//         roll:'005'
+//     },
+//     {
+//         name:'sujal',
+//         roll:'006'
+//     }]
+
+
+// students.forEach((list)=>(console.log(list.name)))
+
+
+
+
+// const students =[
+//     {
+//         name:'shubham',
+//         roll:'001'
+//     },
+//     {
+//         name:'anjali',
+//         roll:'002'
+//     },
+//     {
+//         name:'dipesh',
+//         roll:'003'
+//     },
+//     {
+//         name:'sweety',
+//         roll:'004'
+//     },
+//     {
+//         name:'kajal',
+//         roll:'005'
+//     },
+//     {
+//         name:'sujal',
+//         roll:'006'
+//     }]
+
+// students.map(info =>(info.city = "mumbai"))
+
+// console.log(students)
+
+
+
+
+
+// const stud ={
+//     name:'shubham',
+//     roll:25,
+//     city:'mumbai'
+// }
+
+// delete stud.sal 
+// console.log(stud);
+
+
+
+// const students =[
+//     {
+//         name:'shubham',
+//         roll:'001',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'anjali',
+//         roll:'002',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'dipesh',
+//         roll:'003'
+//     },
+//     {
+//         name:'sweety',
+//         roll:'004'
+//     },
+//     {
+//         name:'kajal',
+//         roll:'005'
+//     },
+//     {
+//         name:'sujal',
+//         roll:'006'
+//     }]
+
+
+// const mumbaiStudent =  students.filter(info => (info.city === "mumbai"))
+
+//     console.log(mumbaiStudent);
+
+
+
+
+// const students =[
+//     {
+//         name:'shubham',
+//         roll:'001',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'anjali',
+//         roll:'002',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'dipesh',
+//         roll:'003'
+//     },
+//     {
+//         name:'sweety',
+//         roll:'004'
+//     },
+//     {
+//         name:'kajal',
+//         roll:'005'
+//     },
+//     {
+//         name:'sujal',
+//         roll:'006'
+//     }]
+
+
+// const mumbaiStudent =  students.some(info => (info.city === "mumbai"))
+
+//     console.log(mumbaiStudent);
+
+
+
+
+
+//     const students =[
+//     {
+//         name:'shubham',
+//         roll:'001',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'anjali',
+//         roll:'002',
+//         city: 'mumbai'
+//     },
+//     {
+//         name:'dipesh',
+//         roll:'003'
+//     },
+//     {
+//         name:'sweety',
+//         roll:'004'
+//     },
+//     {
+//         name:'kajal',
+//         roll:'005'
+//     },
+//     {
+//         name:'sujal',
+//         roll:'006'
+//     }]
+
+
+// const mumbaiStudent =  students.every(info => (info.city === "mumbai"))
+
+//     console.log(mumbaiStudent);
+    
+
