08dad677d773d3bb8132f6e8270bb975e13e59c7
Shubham <vishwakarmashubham202@gmail.com>
2025-06-11 20:49:04 +0530

    learn some new concepts

---
+++ Calculator/jscriptconcept/reduceMethod.js
@@ -0,0 +1,78 @@
+// nums.reduce((accumulator , currentValue) => (accumulator + currentValue),initialValue);
+
+
+// let nums = [1,2,,3,4,5,6];
+
+// let newnums = nums.reduce( (acc , curt) => (acc + curt),0)
+
+// console.log(newnums);
+
+
+// const Cart = [ 
+//     {
+//         product:'Biscuit',
+//         price:169,
+//         brand:'parle'
+//     },
+
+//     {
+//         product:'Biscuit',
+//         price:129,
+//         brand:'vita'
+//     },
+
+//     {
+//         product:'Biscuit',
+//         price:299,
+//         brand:'Oreo'
+//     },
+
+//     {
+//         product:'Choclate',
+//         price:399,
+//         brand:'parle'
+//     },
+
+//     {
+//         product:'Cadbory',
+//         price:599,
+//         brand:'Oreo'
+//     }
+
+// ]
+
+// const Totalprod = Cart.filter((prod) => (prod.product === 'Biscuit'))  
+//                         .reduce((acc , curt) => (acc+curt.price),0)
+ 
+
+// console.log(Totalprod);
+
+
+
+
+
+const nums = [5,9,7,1,3,8,1,7,5,6,8]
+
+// let max = nums.reduce((acc , crnt) => {
+//     let max = 0 ;
+//     for(let i=0; i<=nums.length; i++){
+//         if(max < nums[i]){
+//             max = nums[i];
+
+//             return max;
+//         }
+//     }
+// },0)
+
+// console.log(max);
+
+
+// let maxNum = nums.reduce((acc, crnt) => {
+//     if(acc < crnt){
+//         return crnt;
+//     }else {
+//         return acc ;
+//     }
+// },0)
+
+// console.log(maxNum);
